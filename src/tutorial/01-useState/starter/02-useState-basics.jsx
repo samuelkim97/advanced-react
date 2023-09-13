e3b729be02a07988e60a2ddd39ed2c42b06907a0
Bumsoo Kim <kbeom00@gmail.com>
2023-09-13 19:12:22 +0900

    done 01-02

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button className="btn" onClick={handleClick}>
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
