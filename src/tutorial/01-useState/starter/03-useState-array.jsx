e44f315a005d47585de9c1998b2352bc35a5ddba
Bumsoo Kim <kbeom00@gmail.com>
2023-09-13 21:20:22 +0900

    done 01-03

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,44 @@
+import React from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const removeAll = () => {
+    setPeople([]);
+  };
+  const removeItem = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <button
+              type="button"
+              className="btn"
+              onClick={() => removeItem(id)}
+              style={{ marginBottom: "1rem" }}
+            >
+              remove item
+            </button>
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        className="btn"
+        onClick={removeAll}
+        style={{ marginTop: "1rem" }}
+      >
+        clear all items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
