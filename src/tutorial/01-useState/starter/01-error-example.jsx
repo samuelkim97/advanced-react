2c596c88dcb53a6e3b8c9c86c580b911271f044b
Bumsoo Kim <kbeom00@gmail.com>
2023-09-13 18:11:40 +0900

    done 01-01

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,17 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+  const handleClick = () => {
+    count += 1;
+    console.log(count);
+  };
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={handleClick} className="btn">
+        click me!
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
