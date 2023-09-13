9489b387ea4a361cc719d75df04603e9d831220c
Bumsoo Kim <kbeom00@gmail.com>
2023-09-13 22:52:48 +0900

    done 01-05

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,25 @@
+import React from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [count, setCount] = React.useState(0);
+
+  const increaseCount = () => {
+    setTimeout(() => {
+      setCount((prevCount) => {
+        const newCount = prevCount + 1;
+        return newCount;
+      });
+    }, 3000);
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button type="button" onClick={increaseCount} className="btn">
+        click me!
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
