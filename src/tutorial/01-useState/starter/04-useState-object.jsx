35f98ffc9de6ceda9c5f1099f3c39759d431de1c
Bumsoo Kim <kbeom00@gmail.com>
2023-09-13 22:03:18 +0900

    done 01-04

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,33 @@
-import { useState } from 'react';
+import React from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // const [name, setName] = React.useState("peter");
+  // const [age, setAge] = React.useState(24);
+  // const [hobby, setHobby] = React.useState("read books");
+
+  const [person, setPerson] = React.useState({
+    name: "peter",
+    age: 24,
+    hobby: "read books",
+  });
+
+  const displayPerson = () => {
+    // setName("john");
+    // setAge(37);
+    // setHobby("fight with people");
+    setPerson({ ...person, age: 99 });
+  };
+
+  return (
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <h2>{person.hobby}</h2>
+      <button type="button" className="btn" onClick={displayPerson}>
+        show john
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
