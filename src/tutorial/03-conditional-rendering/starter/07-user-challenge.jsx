45bd9264f2cdc9745fa6c7fe4cec36d348517ea4
Bumsoo Kim <kbeom00@gmail.com>
2023-09-16 22:05:37 +0900

    done 03-07

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+
+  const login = (name) => {
+    setUser({ name: name });
+  };
+
+  const logout = () => {
+    setUser(null);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      {user ? <h4>hello there, {user.name}</h4> : <h4>please login</h4>}
+      {!user && (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+          <br />
+        </>
+      )}
+      {user ? (
+        <button className="btn" onClick={() => logout()}>
+          logout
+        </button>
+      ) : (
+        <button
+          className="btn"
+          onClick={() => {
+            if (!inputValue) {
+              alert("please type your name");
+              return;
+            }
+            login(inputValue);
+          }}
+        >
+          login
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
