9e186b72936ecdba5972caa345219000f755dc8d
Bumsoo Kim <kbeom00@gmail.com>
2023-09-16 20:51:14 +0900

    done 03-04

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text, setText] = useState("");
+  const [name, setName] = useState("susan");
+
+  return (
+    <>
+      <h2>Falsy &&: {text && "hello world"}</h2>
+      <h2>Falsy ||: {text || "hello world"}</h2>
+      <h2>Truthy &&: {name && "hello world"}</h2>
+      <h2>Truthy ||: {name || "hello world"}</h2>
+    </>
+  );
 };
 export default ShortCircuitOverview;
