ba6c9fe4d504c4e31118ab096ac9f55f568584e6
Bumsoo Kim <kbeom00@gmail.com>
2023-09-16 21:39:06 +0900

    done 03-06

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const MusicOff = () => {
+    return (
+      <div>
+        <h2>click a button to play music</h2>
+      </div>
+    );
+  };
+
+  const MusicOn = () => {
+    return (
+      <div>
+        <h2>click a button to stop music</h2>
+      </div>
+    );
+  };
+
+  const MusicPlayer = () => {
+    return (
+      <div>
+        <h1 style={{ fontSize: "5rem", width: "200px", margin: "auto" }}>
+          music player
+        </h1>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {isPlaying ? <MusicOn /> : <MusicOff />}
+      <button className="btn" onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? "stop" : "play"}
+      </button>
+      {isPlaying && <MusicPlayer />}
+    </>
+  );
 };
 
 export default ToggleChallenge;
