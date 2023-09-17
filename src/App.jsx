3a40e90484e41b58e401eb7ea22dc6aa737f37f6
Bumsoo Kim <kbeom00@gmail.com>
2023-09-17 12:07:33 +0900

    done 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
+import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
+import Example from "./tutorial/04-project-structure/starter/Example";
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      <Example />
+      {/* <Starter /> */}
       {/* <Final /> */}
     </div>
   );
