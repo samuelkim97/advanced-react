cb03fbe073a7f99420cbacd7b050edf84a9e9752
Bumsoo Kim <kbeom00@gmail.com>
2023-09-17 11:38:31 +0900

    done 02-05

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
+import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
 
 function App() {
   return (
     <div className="container">
       <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [isOn, setIsOn] = useState(false);
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setIsOn(!isOn)}>
+        toggle
+      </button>
+      {isOn && <SomeComponent />}
+    </div>
+  );
+};
+
+const SomeComponent = () => {
+  useEffect(() => {
+    // const intervalId = setInterval(() => {
+    //   console.log("hello from interval");
+    // }, 1000);
+    // return () => {
+    //   console.log("cleanup function executed");
+    //   clearInterval(intervalId);
+    // };
+
+    const scrollFunc = () => {
+      console.log("you are scrolling");
+    };
+
+    window.addEventListener("click", scrollFunc);
+    return () => window.removeEventListener("click", scrollFunc);
+  }, []);
+
+  return <h2>simple return</h2>;
 };
 
 export default CleanupFunction;
