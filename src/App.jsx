be7bc4fa4858769aabfb1a66789b2fdafcb702fa
Bumsoo Kim <kbeom00@gmail.com>
2023-09-21 23:39:29 +0900

    done 11-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/11-performance/starter/03-hooks";
+import Starter from "./tutorial/11-performance/starter/04-react-18";
 import Navbar from "./tutorial/09-context-api/starter/Navbar";
 
 function App() {
