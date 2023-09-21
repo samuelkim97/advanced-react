6a2111a5a00c085d27e851eb7191992be4110860
Bumsoo Kim <kbeom00@gmail.com>
2023-09-21 21:14:18 +0900

    done 11-02

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/11-performance/starter/01-lower-state";
+import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge";
 import Navbar from "./tutorial/09-context-api/starter/Navbar";
 
 function App() {
