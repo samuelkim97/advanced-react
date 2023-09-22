3d34c440400ce3dd79a47f256876d2b3c77bb047
Bumsoo Kim <kbeom00@gmail.com>
2023-09-22 18:03:04 +0900

    done 11-04

--- src/tutorial/11-performance/starter/04-react-18/index.jsx
+++ src/tutorial/11-performance/starter/04-react-18/index.jsx
@@ -1,42 +1,54 @@
-import { useState } from 'react';
+import { Suspense, lazy, useState, useTransition } from "react";
+const SlowComponent = lazy(() => import("./SlowComponent"));
+
 const LatestReact = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [items, setItems] = useState([]);
+  const [isPending, startTransition] = useTransition();
 
   const handleChange = (e) => {
     setText(e.target.value);
 
-    // slow down CPU
-    // const newItems = Array.from({ length: 5000 }, (_, index) => {
-    //   return (
-    //     <div key={index}>
-    //       <img src='/vite.svg' alt='' />
-    //     </div>
-    //   );
-    // });
-    // setItems(newItems);
+    startTransition(() => {
+      const newItems = Array.from({ length: 5000 }, (_, index) => {
+        return (
+          <div key={index}>
+            <img src="/vite.svg" alt="" />
+          </div>
+        );
+      });
+      setItems(newItems);
+    });
   };
+
   return (
     <section>
-      <form className='form'>
+      <form className="form">
         <input
-          type='text'
-          className='form-input'
+          type="text"
+          className="form-input"
           value={text}
           onChange={handleChange}
         />
       </form>
       <h4>Items Below</h4>
 
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          marginTop: '2rem',
-        }}
-      >
-        {items}
-      </div>
+      {isPending ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            marginTop: "2rem",
+          }}
+        >
+          {items}
+        </div>
+      )}
+      <Suspense fallback={<div>Loading...</div>}>
+        <SlowComponent />
+      </Suspense>
     </section>
   );
 };
